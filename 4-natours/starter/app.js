770d3f0e5426a595296125e9c9fe35b0943566f0
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-04-30 14:31:17 +0300

    add users routs and handlers. Refactor routs

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 
 const app = express();
 
-// middleware to parse req body
-app.use(express.json());
+// it's sync because it's out of even loop
+const toursSimple = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-// reqeust handlers
+// middleware is every function along the way of getting a req, and sending a res
+// so order of calling functions matters a lot
+// without next function the req/res cycle will stuck
+// resulting in not sending anything back
+app.use(morgan("dev"));
+app.use(express.json());
+app.use((req, res, next) => {
+  req.reqTime = new Date().toISOString();
+  next();
+});
+
+// HANDLERS
+// tours
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -109,17 +124,58 @@ const createTour = (req, res) => {
   // res.send("done");
 };
 
-// it's sync because it's out of even loop
-const toursSimple = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// users
 
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
-app
-  .route("/api/v1/tours/:id")
-  .get(getTourById)
-  .patch(updateTour)
-  .delete(deleteTour);
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "rout in development",
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "rout in development",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "rout in development",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "rout in development",
+  });
+};
+
+const getUserById = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "rout in development",
+  });
+};
+
+// ROUTS
+
+const toursRouter = express.Router();
+const usersRouter = express.Router();
+
+// tours
+toursRouter.route("/").get(getAllTours).post(createTour);
+toursRouter.route("/:id").get(getTourById).patch(updateTour).delete(deleteTour);
+
+// users
+usersRouter.route("/").get(getAllUsers).post(createUser);
+usersRouter.route("/:id").get(getUserById).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/tours/", toursRouter);
+app.use("/api/v1/users/", usersRouter);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`listening on port: ${PORT}...`));
